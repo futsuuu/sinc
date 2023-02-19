fd1d6dcd62fd76a5acddeb14de52ea2ff8e61620
futsuuu <futsuuu123@gmail.com>
2023-02-20 06:15:41 +0900

    update
    (-_-;)

--- src/config.rs
+++ src/config.rs
@@ -1,27 +1,24 @@
 use std::{env, fs, io::Error};
 
-use dirs::config_dir;
+use dirs::{config_dir, home_dir};
 use serde::Deserialize;
 use toml::{value, Value};
 
-#[derive(Deserialize)]
-struct UserConfig {
-    dotfiles: value::Array,
-}
+mod dotfile;
 
 #[derive(Debug)]
 pub struct Config {
-    pub dotfiles: Vec<Dotfile>,
-}
-
-#[derive(Debug)]
-pub struct Dotfile {
-    pub config: String,
-    pub path: String,
-    pub link: bool,
+    pub dir: String,
+    pub dotfiles: Vec<dotfile::Dotfile>,
 }
 
 pub fn load_config() -> Result<Config, Error> {
+    #[derive(Deserialize)]
+    struct UserConfig {
+        dir: String,
+        dotfiles: value::Array,
+    }
+
     let user_config: UserConfig = {
         let s = fs::read_to_string(
             config_dir()
@@ -34,28 +31,38 @@ pub fn load_config() -> Result<Config, Error> {
         toml::from_str(&s).unwrap()
     };
 
-    let mut dotfiles = Vec::new();
-    for dotfile in user_config.dotfiles {
-        let config = get_item("config", &dotfile, &Value::from(""))
-            .to_string()
-            .trim_matches('"')
-            .to_string();
-        let path = get_item("path", &dotfile, &Value::from("~"))
-            .to_string()
-            .trim_matches('"')
-            .to_string();
-        let link = get_item("link", &dotfile, &Value::from(true))
-            .as_bool()
-            .unwrap();
+    let dir = expand_home(user_config.dir);
 
-        dotfiles.push(Dotfile { config, path, link })
+    let mut dotfiles = Vec::new();
+    for df in user_config.dotfiles {
+        let path = get_item("path", &df, &Value::from(""))
+            .to_string()
+            .trim_matches('"')
+            .to_string();
+        let target = get_item("target", &df, &Value::from(""))
+            .to_string()
+            .trim_matches('"')
+            .to_string();
+        let sync_type = match get_item("type", &df, &Value::from("symlink")).as_str() {
+            Some("symlink") => dotfile::SyncType::SymLink,
+            Some("hardlink") => dotfile::SyncType::HardLink,
+            Some("junction") => dotfile::SyncType::Junction,
+            Some("copy") => dotfile::SyncType::Copy,
+            _ => dotfile::SyncType::SymLink,
+        };
+
+        dotfiles.push(dotfile::Dotfile::new(
+            format!("{}/{}", &dir, path).replace("/", "\\"),
+            expand_home(target),
+            sync_type,
+        ));
     }
 
-    Ok(Config { dotfiles })
+    Ok(Config { dir, dotfiles })
 }
 
-fn get_item<'a>(item_name: &'a str, dotfile: &'a Value, default: &'a Value) -> &'a Value {
-    match dotfile.get(item_name) {
+fn get_item<'a>(item_name: &'a str, val: &'a Value, default: &'a Value) -> &'a Value {
+    match val.get(item_name) {
         Some(item) => match item {
             Value::Table(t) => match t.get(env::consts::OS) {
                 Some(val) => val,
@@ -64,5 +71,16 @@ fn get_item<'a>(item_name: &'a str, dotfile: &'a Value, default: &'a Value) -> &
             _ => item,
         },
         None => default,
+    }
+}
+
+fn expand_home(path: String) -> String {
+    if path.starts_with('~') {
+        let mut p = home_dir().unwrap();
+        let home: &[_] = &['~', '/', '\\'];
+        p.push(path.trim_start_matches(home).to_string());
+        p.display().to_string()
+    } else {
+        path
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use std::io::Error;
+use std::{fs, io::Error, path::PathBuf};
 
 mod config;
 
 fn main() -> Result<(), Error> {
-    let config = config::load_config()?;
-    println!("{:#?}", config);
+    let config_data = config::load_config()?;
+    let dotfiles_dir = PathBuf::from(&config_data.dir);
+    if !dotfiles_dir.exists() {
+        fs::create_dir_all(&dotfiles_dir)?;
+    }
+
+    for dotfile in config_data.dotfiles {
+        dotfile.sync()?;
+    }
+
     Ok(())
 }
