f478b8b66bbcc7da9ed0cc792210fa0ef002c109
futsuuu <futsuuu123@gmail.com>
2023-03-25 17:57:26 +0900

    refactor: isolate the functions of Dotfile

--- src/dotfile.rs
+++ src/dotfile.rs
@@ -5,7 +5,7 @@ use std::os::windows;
 use std::{
     fs,
     io::Write,
-    path::PathBuf,
+    path::{Path, PathBuf},
     process::{Command, ExitStatus, Stdio},
 };
 
@@ -66,137 +66,136 @@ impl Dotfile {
     pub fn sync(&self) {
         print!("{}", ui::title(self.name.clone()));
         if self.enable {
-            if let Err(e) = self._sync() {
+            if let Err(e) = sync(&self.path, &self.target, &self.sync_type) {
                 println!("{}", e)
-            }
-        } else {
-            self.print_message()
-        }
-    }
-
-    fn _sync(&self) -> Result<()> {
-        if self.target.exists() {
-            if self.path.exists() {
-                // o -> o
-                let target = &self.target;
-                let new_target = PathBuf::from(self.target.display().to_string() + ".old_version");
-
-                fs::rename(&self.target, &new_target).or(Err(SyncError::FsProcessingError))?;
-                match self.new_item() {
-                    Ok(_) => {
-                        fs_extra::remove_items(&[&new_target]).or(Err(SyncError::FsProcessingError))
-                    }
-                    Err(e) => {
-                        fs::rename(new_target, target).or(Err(SyncError::FsProcessingError))?;
-                        Err(e)
-                    }
-                }?;
             } else {
-                // x -> o
-                let parent_dir = self.path.parent().unwrap();
-                if !parent_dir.exists() {
-                    fs::create_dir_all(parent_dir).or(Err(SyncError::FsProcessingError))?;
+                print_message(&self.path, &self.target, &self.sync_type);
+
+                if !&self.hook_add.is_empty() {
+                    match run_command(&self.hook_add) {
+                        Ok(exit_status) => {
+                            if let Some(code) = exit_status.code() {
+                                println!("Exit with status code {}.", code);
+                            } else {
+                                println!("Process terminated by signal.");
+                            }
+                        }
+                        Err(e) => println!("{}", e),
+                    }
                 }
-                fs::rename(&self.target, &self.path).or(Err(SyncError::FsProcessingError))?;
-                self.new_item()?;
-            };
-        } else if self.path.exists() {
-            // o -> x
-            self.new_item()?;
-        } else {
-            return Err(SyncError::NotFound {
-                path0: self.path.clone(),
-                path1: self.target.clone(),
-            });
-        };
-        if !&self.hook_add.is_empty() {
-            if let Some(code) = run_command(&self.hook_add)?.code() {
-                println!("Exit with status code {}.", code);
-            } else {
-                println!("Process terminated by signal.")
             }
-        }
-
-        Ok(())
-    }
-
-    pub fn print_message(&self) {
-        if self.enable {
-            println!(
-                "{}{}{}{}{}",
-                ui::path(&self.path),
-                ui::symbol(" ====( "),
-                ui::item_type(self.sync_type.clone()),
-                ui::symbol(" )===> "),
-                ui::path(&self.target),
-            )
         } else {
             println!("disable: {}", ui::path(&self.path));
         }
     }
+}
 
-    fn new_item(&self) -> Result<()> {
-        match self.sync_type.as_str() {
-            "symlink" => {
-                self.create_symlink()?;
-            }
-            "hardlink" => {
-                fs::hard_link(&self.path, &self.target).or(Err(SyncError::FsProcessingError))?;
-            }
-            "junction" => {
-                if cfg!(target_os = "windows") {
-                    Command::new("cmd")
-                        .arg("/C")
-                        .args([
-                            "mklink",
-                            "/J",
-                            self.target.to_str().unwrap(),
-                            self.path.to_str().unwrap(),
-                        ])
-                        .stdout(Stdio::null())
-                        .spawn()
-                        .or(Err(SyncError::CommandError))?;
-                } else {
-                    return Err(SyncError::Unsupported {
-                        error_type: self.sync_type.clone(),
-                    });
+fn print_message(path: &Path, target: &Path, sync_type: &str) {
+    println!(
+        "{}{}{}{}{}",
+        ui::path(path),
+        ui::symbol(" ====( "),
+        ui::item_type(sync_type),
+        ui::symbol(" )===> "),
+        ui::path(target),
+    )
+}
+
+fn sync(path: &PathBuf, target: &PathBuf, sync_type: &str) -> Result<()> {
+    if target.exists() {
+        if path.exists() {
+            // o -> o
+            let target = &target;
+            let new_target = PathBuf::from(target.display().to_string() + ".old_version");
+
+            fs::rename(target, &new_target).or(Err(SyncError::FsProcessingError))?;
+            match new_item(path, target, sync_type) {
+                Ok(_) => {
+                    fs_extra::remove_items(&[&new_target]).or(Err(SyncError::FsProcessingError))
                 }
-            }
-            "copy" => {
-                fs_extra::copy_items(
-                    &[&self.path],
-                    &self.target,
-                    &CopyOptions::new().copy_inside(true),
-                )
-                .or(Err(SyncError::FsProcessingError))?;
-            }
-            _ => (),
-        }
-        self.print_message();
-        Ok(())
-    }
-
-    #[cfg(not(target_os = "windows"))]
-    fn create_symlink(&self) -> Result<()> {
-        unix::fs::symlink(&self.path, &self.target).or(Err(SyncError::FsProcessingError))?;
-        Ok(())
-    }
-
-    #[cfg(target_os = "windows")]
-    fn create_symlink(&self) -> Result<()> {
-        unsafe {
-            if IsUserAnAdmin() == 0 {
-                return Err(SyncError::NoAdministrator);
-            }
-        }
-        if self.target.is_dir() {
-            windows::fs::symlink_dir(&self.path, &self.target)
+                Err(e) => {
+                    fs::rename(new_target, target).or(Err(SyncError::FsProcessingError))?;
+                    Err(e)
+                }
+            }?;
         } else {
-            windows::fs::symlink_file(&self.path, &self.target)
+            // x -> o
+            let parent_dir = path.parent().unwrap();
+            if !parent_dir.exists() {
+                fs::create_dir_all(parent_dir).or(Err(SyncError::FsProcessingError))?;
+            }
+            fs::rename(target, path).or(Err(SyncError::FsProcessingError))?;
+            new_item(path, target, sync_type)?;
+        };
+    } else if path.exists() {
+        // o -> x
+        new_item(path, target, sync_type)?;
+    } else {
+        return Err(SyncError::NotFound {
+            path0: path.clone(),
+            path1: target.clone(),
+        });
+    };
+
+    Ok(())
+}
+
+fn new_item(path: &PathBuf, target: &PathBuf, sync_type: &str) -> Result<()> {
+    match sync_type {
+        "symlink" => {
+            create_symlink(path, target)?;
         }
-        .or(Err(SyncError::FsProcessingError))?;
-        Ok(())
+        "hardlink" => {
+            fs::hard_link(path, target).or(Err(SyncError::FsProcessingError))?;
+        }
+        "junction" => {
+            if cfg!(target_os = "windows") {
+                Command::new("cmd")
+                    .arg("/C")
+                    .args([
+                        "mklink",
+                        "/J",
+                        target.to_str().unwrap(),
+                        path.to_str().unwrap(),
+                    ])
+                    .stdout(Stdio::null())
+                    .spawn()
+                    .or(Err(SyncError::CommandError))?;
+            } else {
+                return Err(SyncError::Unsupported {
+                    error_type: sync_type.to_string(),
+                });
+            }
+        }
+        "copy" => {
+            fs_extra::copy_items(&[path], target, &CopyOptions::new().copy_inside(true))
+                .or(Err(SyncError::FsProcessingError))?;
+        }
+        _ => (),
     }
+    Ok(())
+}
+
+#[cfg(not(target_os = "windows"))]
+fn create_symlink(path: &PathBuf, target: &PathBuf) -> Result<()> {
+    unix::fs::symlink(path, target).or(Err(SyncError::FsProcessingError))?;
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+fn create_symlink(path: &PathBuf, target: &PathBuf) -> Result<()> {
+    unsafe {
+        if IsUserAnAdmin() == 0 {
+            return Err(SyncError::NoAdministrator);
+        }
+    }
+    if target.is_dir() {
+        windows::fs::symlink_dir(path, target)
+    } else {
+        windows::fs::symlink_file(path, target)
+    }
+    .or(Err(SyncError::FsProcessingError))?;
+    Ok(())
 }
 
 fn run_command(command: &str) -> Result<ExitStatus> {
